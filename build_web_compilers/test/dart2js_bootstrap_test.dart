bc18597bbf34df8758ff4240f458da48aaafd43f
Kevin Moore <kevmoo@google.com>
2018-06-01 14:08:47 -0700

    5x?

--- build_web_compilers/test/dart2js_bootstrap_test.dart
+++ build_web_compilers/test/dart2js_bootstrap_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@Timeout.factor(2)
+@Timeout.factor(5)
 
 import 'package:build_test/build_test.dart';
 import 'package:test/test.dart';
